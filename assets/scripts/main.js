724a321ef9794b1098aa2a7343e80c9e5ee47e15
Luiz Fernando Souza <fenna.wake@gmail.com>
2022-04-11 11:42:51 +1000

    First commit with structure and some section ready | nav menu ready

---
+++ assets/scripts/main.js
@@ -0,0 +1,31 @@
+
+/**  toggle menu  */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/**  hidden menu when click in any menu item  */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+/** add shadow to header when scrolling page */
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll')
+    }
+})
